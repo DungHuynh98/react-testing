e56563a1f19278cf7bbe64eda3c673641e3cd590
Dung Huynh <dunghuynh.010598@gmail.com>
2024-07-12 21:59:26 +0700

    Update button css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { useState } from 'react';
-import './App.css';
-import UserList from './components/UserList';
+import { useState } from "react";
+import "./App.css";
+import UserList from "./components/UserList";
 
 function App() {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   return (
     <div className="App">
-      {
-        show ? <UserList /> : <button onClick={() => setShow(true)} style={{ marginTop: 20 }}>Get users</button>
-      }
+      {show ? (
+        <UserList />
+      ) : (
+        <button
+          onClick={() => setShow(true)}
+          style={{
+            marginTop: 20,
+            padding: "8px 10px",
+            border: "none",
+            background: "#000",
+            color: "#fff",
+            fontSize: 16,
+            fontWeight: "bold",
+            borderRadius: 8,
+            cursor: "pointer",
+          }}
+        >
+          Get users
+        </button>
+      )}
     </div>
   );
 }
